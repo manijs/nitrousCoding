5a86861be73a99eb81b915ba6f22d8af4b179722
manijs <temptesting007@gmail.com>
2016-03-01 11:03:26 +0000

    Modified server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hello world from nitrous online ide");
+// console.log("hello world from nitrous online ide");
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
+
+server.listen(8080, "0.0.0.0");
+console.log("Server is listening");
